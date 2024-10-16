eb4a025410615d4bf0e0661a45437dff28dcd1d9
Chatelet Ferrety Matias <matias.chatelet-ferrety@etu.uni-lyon1.fr>
2024-10-16 17:18:32 +0200

    feat: :sparkles: Add Services

---
+++ text-generation-service/src/main/java/org/example/textgenerationservice/TextGenerationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.textgenerationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextGenerationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextGenerationServiceApplication.class, args);
+    }
+
+}
