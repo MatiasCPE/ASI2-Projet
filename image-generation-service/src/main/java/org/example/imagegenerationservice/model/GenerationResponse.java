fdc2bf505efa33d9cdb885d8741812db44b1deb0
Chatelet Ferrety Matias <matias.chatelet-ferrety@etu.uni-lyon1.fr>
2024-10-31 20:26:26 +0100

    feat: :sparkles: Text & Image Generator

---
+++ image-generation-service/src/main/java/org/example/imagegenerationservice/model/GenerationResponse.java
@@ -0,0 +1,2 @@
+package org.example.imagegenerationservice.model;public class GenerationResponse {
+}
---
+++ text-generation-service/src/main/java/org/example/textgenerationservice/model/GenerationRequest.java
@@ -0,0 +1,15 @@
+package org.example.textgenerationservice.dto;
+
+public class GenerationRequest {
+    private String prompt;
+
+    // Getters et Setters
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+}
